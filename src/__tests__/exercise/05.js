e5b847b8ba62965be9bf950155bc37775c242b01
yagnesh parekh <yagneshparekh@rocketmail.com>
2025-12-06 05:46:16 -0500

    server fail test

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -23,8 +23,8 @@ const buildLoginForm = build({
 const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
-// afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -62,3 +62,32 @@ test(`error when username not provided`, async () => {
     `"username required"`,
   )
 })
+
+test(`error when server is down`, async () => {
+  const testErrorMessage = 'oops!something went wrong '
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+  render(<Login />)
+
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  // screen.debug()
+  expect(screen.getByRole('alert').toHaveTextContent(testErrorMessage))
+})
+
+/**
+ * My Notes:
+ *
+ * 1. server.use(...) is used to override existing request handlers in MSW (Mock Service Worker) during tests. This allows you to simulate different server responses without changing the original handlers.
+ *
+ * 2. In the provided code snippet, server.use(...) is employed to create a custom handler for the login POST request that simulates a server error (HTTP 500 status) with a specific error message. This is useful for testing how the application handles server errors during login attempts.
+ *
+ * 3.server.resetHandlers() is a method in MSW that resets all request handlers to their original state. This is particularly useful in testing scenarios where you want to ensure that any modifications made to the handlers during a test do not affect subsequent tests.
+ * 4. if you move server failing test above other tests then those tests will also fail because the server will continue to use the overridden handler until resetHandlers() is called in afterEach.
+ */
