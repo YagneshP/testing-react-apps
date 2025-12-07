84a5e9f208966a3f8008f4109cd4a90bd748973e
yagnesh parekh <yagneshparekh@rocketmail.com>
2025-12-07 07:16:48 -0500

    dark theme test

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -19,4 +19,15 @@ test('renders with the light styles for the light theme', () => {
   `)
 })
 
+test('renders with the dark styles for the dark theme', () => {
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  )
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    color: white;
+    background-color: black;
+  `)
+})
 /* eslint no-unused-vars:0 */
